915cd522790be4e234bc46aaa0b09898f1691d15
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-12-25 12:26:52 +0530

    str change

--- src/components/upload_video/progress_bar.js
+++ src/components/upload_video/progress_bar.js
@@ -17,7 +17,7 @@ const ProgressBar = ({ progress }) => {
         aria-valuemin="0"
         aria-valuemax="100"
       >
-        {progress < 100 ? `${progress}%` : "Uploaded!"}
+        {progress < 100 ? `${progress}%` : "Upload complete!"}
       </div>
     </div>
   );
