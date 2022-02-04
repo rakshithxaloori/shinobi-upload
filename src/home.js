144135ea588446715980afd51bdb81c0dc9919fc
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2022-02-04 22:36:25 +0530

    verify video types

--- src/home.js
+++ src/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useAlert } from "react-alert";
 
 import "./styles/home.css";
 
@@ -7,17 +8,31 @@ import BrowseVideo from "./components/browse_video";
 import UploadVideo from "./components/upload_video";
 
 import UserContext from "./userContext";
+import { VIDEO_MAX_SIZE, VIDEO_MIME_TYPES } from "./utils";
 
 const Home = () => {
+  const alert = useAlert();
   const context = React.useContext(UserContext);
   const { user } = context;
 
   const [videoUrl, setVideoUrl] = React.useState(null);
   const [videoInfo, setVideoInfo] = React.useState(null);
 
-  const updateSelectedVideo = (newVideoInfo) => {
-    setVideoUrl(URL.createObjectURL(newVideoInfo) || null);
-    setVideoInfo(newVideoInfo);
+  const updateSelectedVideo = async (videoFile) => {
+    // TODO if videoFile.type not VIDEO_MIME_TYPES, convert
+    // Else upload directly
+    if (VIDEO_MIME_TYPES.includes(videoFile.type) === false) {
+      alert.show("Select an ogg, mp4, mov or webm video");
+      return;
+    }
+    if (videoFile.size > VIDEO_MAX_SIZE) {
+      alert.show(
+        `Choose a video smaller than ${VIDEO_MAX_SIZE / (1000 * 1000)} MB`
+      );
+      return;
+    }
+    setVideoUrl(URL.createObjectURL(videoFile) || null);
+    setVideoInfo(videoFile);
   };
 
   return (
